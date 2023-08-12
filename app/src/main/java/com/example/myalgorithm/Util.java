11c1241dde52bc15fe383bcc1a1f09e194c4157f
jinguangyue <jinguangyue@jinguangyue.local>
2023-08-12 10:12:12 +0800

    数组加1算法

--- app/src/main/java/com/example/myalgorithm/Util.java
+++ app/src/main/java/com/example/myalgorithm/Util.java
@@ -198,4 +198,24 @@ public class Util {
         }
     }
 
+
+    /**
+     * 数组加一算法
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        for (int i=digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
 }
