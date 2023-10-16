61faf1361837545e2085064f53d516037cba8136
jinguangyue <jinguangyue@jinguangyue.local>
2023-10-16 16:12:44 +0800

    迭代方法链表翻转

--- app/src/main/java/com/example/myalgorithm/Util.java
+++ app/src/main/java/com/example/myalgorithm/Util.java
@@ -767,6 +767,30 @@ public class Util {
 
             return result;
         }
+
+
+        /**
+         * 1-2-3-4-5-6
+         * @param head
+         * @return
+         */
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode prev = null;
+            ListNode current = head;
+
+            while (current != null) {
+                ListNode next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
     }
 
 }
