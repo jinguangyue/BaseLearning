d42006a3c9eee32a4c58545c7fcf2030fe3b2816
jinguangyue <1010398694@qq.com>
2023-10-16 21:14:10 +0800

    迭代方式处理有序链表升序排列

--- app/src/main/java/com/example/myalgorithm/Util.java
+++ app/src/main/java/com/example/myalgorithm/Util.java
@@ -786,18 +786,43 @@ public class Util {
 
     }
 
+    /**
+     * 124
+     * 134
+     * @param list1
+     * @param list2
+     * @return
+     */
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if (list1 == null) {
-            return list2;
-        } else if (list2 == null) {
-            return list1;
-        } else if (list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list2.next, list1);
-            return list2;
+//        if (list1 == null) {
+//            return list2;
+//        } else if (list2 == null) {
+//            return list1;
+//        } else if (list1.val <= list2.val) {
+//            list1.next = mergeTwoLists(list1.next, list2);
+//            return list1;
+//        } else {
+//            list2.next = mergeTwoLists(list2.next, list1);
+//            return list2;
+//        }
+
+        ListNode prev = new ListNode(-1);
+        ListNode preload = prev;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+
+            prev = prev.next;
+
         }
+
+        prev.next = (list1 == null) ? list2 : list1;
+        return preload.next;
 
     }
 
