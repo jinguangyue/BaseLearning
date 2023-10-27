d1305383733727810cda99a9d220f70895074f18
jinguangyue <1010398694@qq.com>
2023-10-27 19:54:16 +0800

    计算机基础学习：使用队列迭做二叉树层序遍历

--- app/src/main/java/com/example/myalgorithm/Util.java
+++ app/src/main/java/com/example/myalgorithm/Util.java
@@ -1030,6 +1030,42 @@ public class Util {
         return true;
     }
 
+    /**
+     * 使用队列做二叉树的层序遍历
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    level.add(node.val);
+
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+            }
+
+            result.add(level);
+        }
+
+        return result;
+    }
+
 }
 
 
