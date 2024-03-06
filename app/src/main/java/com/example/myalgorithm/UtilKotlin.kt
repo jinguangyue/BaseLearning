b6c666e5187f632fb1f5f3f5ecb7dfc48b022252
jinguangyue <1010398694@qq.com>
2024-03-06 17:51:06 +0800

    计算机基础学习：冒泡排序进一步优化，增加变量判断这一轮遍历是否有过swap，如果没有说明已经排好序了 再优化：记录最后排序的位置，每次循环只循环到最后一次变化的位置即可

--- app/src/main/java/com/example/myalgorithm/UtilKotlin.kt
+++ app/src/main/java/com/example/myalgorithm/UtilKotlin.kt
@@ -3,18 +3,34 @@ package com.example.myalgorithm
 class UtilKotlin {
     public fun bubbleSort(arr: IntArray) {
 
-        var isSwap = false
+        var indexOfLastUnSortElement = arr.size - 1
+        var isSwap = true
+        var lastSwapIndex = -1
 
-        for (i in 0 until arr.size) {
-            for (j in 0 until arr.size - i - 1) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(arr, j, j + 1)
+        while (isSwap) {
+            isSwap = false
+            for (i in 0 until indexOfLastUnSortElement) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1)
                     isSwap = true
+                    lastSwapIndex = i
                 }
             }
 
-            if (!isSwap) break
+            indexOfLastUnSortElement = lastSwapIndex
         }
+
+
+//        for (i in 0 until arr.size) {
+//            for (j in 0 until arr.size - i - 1) {
+//                if (arr[j] > arr[j + 1]) {
+//                    swap(arr, j, j + 1)
+//                    isSwap = true
+//                }
+//            }
+//
+//            if (!isSwap) break
+//        }
 
         printArray(arr)
     }
