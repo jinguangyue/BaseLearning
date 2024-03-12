cfb2230d099c79e4f4d37feb5764a967463aba2a
jinguangyue <1010398694@qq.com>
2024-03-12 12:37:26 +0800

    计算机基础学习：删除链表节点 1 使用双指针方式删除链表倒数第n个节点 2 使用栈删除链表第n个节点

--- app/src/main/java/com/example/myalgorithm/ListNodeUtil.kt
+++ app/src/main/java/com/example/myalgorithm/ListNodeUtil.kt
@@ -1,5 +1,8 @@
 package com.example.myalgorithm
 
+import java.util.Deque
+import java.util.LinkedList
+
 class ListNodeUtil {
     companion object {
 
@@ -8,24 +11,70 @@ class ListNodeUtil {
          * 删除链表倒数第n个节点
          */
         fun deleteNodeForIndex(node: ListNode, index: Int) : ListNode {
+//            var dummy = ListNode(0, node)
+//            var cur = node
+//            var count = 0
+//            while (cur != null) {
+//                count++
+//                if (cur.next == null) break
+//                cur = cur.next
+//            }
+//
+//            println("count:${count}")
+//
+//            var head = dummy.next
+//            for (i in 0 until count - index - 1) {
+//                head = head.next
+//            }
+//            println("head:${head.`val`}")
+//            head.next = head.next.next
+//            return dummy.next
+
+
+            /**
+             * 双指针法
+             */
+//            var dummy = ListNode(0, node)
+//            var first = node
+//            var last = dummy
+//
+//            for (i in 0 until index) {
+//                first = first.next
+//            }
+//
+//            while (first != null) {
+//                last = last.next
+//
+//                if (first.next == null) break
+//                first = first.next
+//            }
+//
+//            last.next = last.next.next
+//
+//            return dummy.next
+
+
+            /**
+             * 使用栈删除链表倒数第n个节点
+             */
             var dummy = ListNode(0, node)
-            var cur = node
-            var count = 0
-            while (cur != null) {
-                count++
-                if (cur.next == null) break
-                cur = cur.next
-            }
-
-            println("count:${count}")
-
-            var head = dummy.next
-            for (i in 0 until count - index - 1) {
+            var stack: Deque<ListNode> = LinkedList()
+            var head = node
+            while (head != null) {
+                stack.push(head)
+                if (head.next == null) break
                 head = head.next
             }
-            println("head:${head.`val`}")
-            head.next = head.next.next
+
+
+
+            for (i in 0 until index) {
+                stack.pop()
+            }
+            var cur: ListNode = stack.peekFirst()
+            cur.next = cur.next.next
             return dummy.next
+
         }
 
 
