c0271ea6022db8f51abbb5d6834cf4d16685c14a
jinguangyue <1010398694@qq.com>
2024-03-16 13:51:08 +0800

    计算机基础学习：java公平锁和线程

---
+++ app/src/main/java/com/example/myalgorithm/WaitDemo.java
@@ -0,0 +1,58 @@
+package com.example.myalgorithm;
+
+public class WaitDemo {
+
+    private String inputString = null;
+    private final Object lock = new Object();
+
+    private void setInputString() {
+        inputString = "jinguangyue";
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+
+    private void readString() {
+        try {
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("inputString===" + inputString);
+    }
+
+
+    public static void main(String[] args) {
+
+        WaitDemo demo = new WaitDemo();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                demo.setInputString();
+            }
+        }).start();
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                demo.readString();
+            }
+        }).start();
+    }
+}
